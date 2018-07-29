bc682e52f55c4db181784668e19617a49909f286
Yuyuko <40657629+Yuyuko-sama@users.noreply.github.com>
2018-07-29 18:25:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,96 +1,53 @@
-// <3 Pinkie Pie :3 - fork by Haku, Includes skip-cutscenes for problematic zones
-module.exports = function QuickLoad(mod) {
-	let lastZone = -1,
-		quick = false,
-		modified = false,
-		lastLocation = null,
-		correctLocation = null,
-		correctAngle = null;
+'use strict'
 
-	mod.command.add(['ql','quickload'], () => {
+let DefaultSettings = {
+	"enabled": true,
+	"loadDistance": 1000,
+	"loadExtra": true,
+	"loadExtraMs": 1000,
+	"blockedZones": [
+		110,
+		111,
+		112,
+		113,
+		114,
+		115,
+		116,
+		117,
+		118,
+		110,
+		9920,
+		9720,
+		9950
+	],
+	"skipCutscenes": false,
+	"skipCutscenesZones": [
+		9735,
+		9935
+	]
+}
 
-		mod.settings.enabled = !mod.settings.enabled;
-		mod.command.message('Module ' + (mod.settings.enabled ? 'en' : 'dis') + 'abled')
-	});
+module.exports = function MigrateSetting(from_ver, to_ver, settings) {
+	if(from_ver === undefined) {
+		return Object.assign(Object.assign({}, DefaultSettings), settings);
+	} else if(from_ver === null) {
+		return DefaultSettings;
+	} else {
+		
+        if (from_ver + 1 < to_ver) {
 
-	mod.game.on('enter_game', () => {
-		lastZone = -1;
-		lastLocation = null;
-	})
+            settings = MigrateSettings(from_ver, from_ver + 1, settings);
+            return MigrateSettings(from_ver + 1, to_ver, settings);
+        }
 
-	mod.hook('S_LOAD_TOPO', 3, {order: 100}, event => {
-		quick = event.quick;
-		if(mod.settings.enabled && event.zone === lastZone && (mod.settings.loadExtra || event.loc.dist3D(lastLocation) <= mod.settings.loadDistance) && !mod.settings.blockedZones.includes(event.zone)) {
-			return modified = event.quick = true; 
-		    };
+        switch(to_ver)
+        {
+            case 2:
+             settings.enabled = true;
+             break;
+        }
+        
+        return settings;
 
-		lastZone = event.zone;
-		modified = false;
-	});
-
-	mod.hook('S_SPAWN_ME', 3, {order: 100}, event => {
-		if(!quick) {
-			correctLocation = event.loc;
-			correctAngle = event.w;
-		};
-
-		if(modified) {
-			if(!lastLocation || event.loc.dist3D(lastLocation) > mod.settings.loadDistance) {
-				process.nextTick(() => { mod.send('S_ADMIN_HOLD_CHARACTER', 2, {hold: true}) })
-			}
-			else modified = false;
-
-			mod.send('S_SPAWN_ME', 3, event) // Bring our character model back from the void
-			mod.send('C_PLAYER_LOCATION', 5, { // Update our position on the server
-				loc: event.loc,
-				w: event.w,
-				lookDirection: 0,
-				dest: event.loc,
-				type: 7,
-				jumpDistance: 0,
-				inShuttle: 0,
-				time: 0
-			});
-		};
-	});
-
-	mod.hook('S_ADMIN_HOLD_CHARACTER', 'raw', () => !modified && undefined);
-
-	mod.hook('C_PLAYER_LOCATION', 5, event => {
-		if(correctLocation) {
-			// Did we accidentally spawn under the map? Let's fix that!
-			if(event.loc.z !== correctLocation.z) {
-				mod.send('S_INSTANT_MOVE', 3, {
-					gameId: mod.game.me.gameId,
-					loc: correctLocation,
-					w: correctAngle
-				});
-				correctLocation = null;
-				return false;
-			};
-			correctLocation = null;
-		};
-	});
-
-	mod.hook('C_PLAYER_LOCATION', 5, {order: 100, filter: {fake: null}}, event => {
-		lastLocation = event.loc;
-	});
-
-	mod.hook('C_VISIT_NEW_SECTION', 'raw', () => {
-		// If our client doesn't send C_PLAYER_LOCATION before this packet, then it's most likely user input
-		correctLocation = null;
-
-		if(modified) {
-			setTimeout(() => { mod.send('S_ADMIN_HOLD_CHARACTER', 2, {hold: false}) }, mod.settings.loadExtraMs);
-			modified = false;
-		}
-	})
-	mod.hook('S_PLAY_MOVIE', 1, {order: 100}, event => {
-		if(mod.settings.skipCutscenesZones.includes(lastZone) && mod.settings.skipCutscenes && mod.settings.enabled) {
-			
-			mod.send('C_END_MOVIE', 1, Object.assign({ unk: true }, event));
-			return false;
-		};
-	});
-	this.destructor = () => {mod.command.remove(['ql','quickload'])}
-};
+	}
+}
