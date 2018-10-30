1e929bdcc996c41f28192d097860d34d2f3d0c45
Yuyuko <40657629+Yuyuko-sama@users.noreply.github.com>
2018-10-30 18:44:07 +0800

    Add files via upload

---
+++ setting_migration.js
@@ -0,0 +1,54 @@
+'use strict'
+
+let DefaultSettings = {
+	"enabled": true,
+	"loadDistance": 1000,
+	"loadExtra": true,
+	"loadExtraMs": 3000,
+	"blockedZones": [
+		110,
+		111,
+		112,
+		113,
+		114,
+		115,
+		116,
+		117,
+		118,
+		110,
+		9920,
+		9720,
+		9950,
+		9066
+	],
+	"skipCutscenes": false,
+	"skipCutscenesZones": [
+		9735,
+		9935
+	]
+}
+
+module.exports = function MigrateSettings(from_ver, to_ver, settings) {
+	if(from_ver === undefined) {
+		return Object.assign(Object.assign({}, DefaultSettings), settings);
+	} else if(from_ver === null) {
+		return DefaultSettings;
+	} else {
+		
+        if (from_ver + 1 < to_ver) {
+
+            settings = MigrateSettings(from_ver, from_ver + 1, settings);
+            return MigrateSettings(from_ver + 1, to_ver, settings);
+        }
+
+        switch(to_ver)
+        {
+            case 2:
+             settings.enabled = true;
+             break;
+        }
+        
+        return settings;
+
+	}
+}
